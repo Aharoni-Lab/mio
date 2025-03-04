e4810b12b5abf0b938c9071f95a8da52791b31d4
MarcelMB <marcelbrosch.mb@gmail.com>
2025-03-03 18:14:18 -0800

    fixed model_frames test to include fourcc="XVID"

--- tests/test_models/test_model_frames.py
+++ tests/test_models/test_model_frames.py
@@ -5,15 +5,18 @@ from pathlib import Path
 
 from mio.models.frames import NamedFrame, NamedVideo
 
+
 class TestNamedFrame(unittest.TestCase):
 
     def setUp(self):
         # Create a single frame (image) and a list of frames (video)
         self.image_frame = np.random.randint(0, 256, (100, 100), dtype=np.uint8)
-        self.video_frames = [np.random.randint(0, 256, (100, 100), dtype=np.uint8) for _ in range(10)]
+        self.video_frames = [
+            np.random.randint(0, 256, (100, 100), dtype=np.uint8) for _ in range(10)
+        ]
         self.name = "test"
 
-    @patch('cv2.imwrite')
+    @patch("cv2.imwrite")
     def test_export_image_frame(self, mock_imwrite):
         # Create instance of NamedFrame with a single image
         named_frame = NamedFrame(name=self.name, frame=self.image_frame)
@@ -22,7 +25,7 @@ class TestNamedFrame(unittest.TestCase):
         # Check that cv2.imwrite was called correctly
         mock_imwrite.assert_called_once_with("output_path_test.png", self.image_frame)
 
-    @patch('mio.models.frames.VideoWriter.init_video')
+    @patch("mio.models.frames.VideoWriter.init_video")
     def test_export_video_frame(self, mock_init_video):
         # Create a mock writer instance
         mock_writer = MagicMock()
@@ -38,14 +41,15 @@ class TestNamedFrame(unittest.TestCase):
             path=Path("output_path_test.avi"),
             width=self.video_frames[0].shape[1],
             height=self.video_frames[0].shape[0],
-            fps=20
+            fps=20,
+            fourcc="XVID",
         )
 
         # Check that the writer's write method was called for each frame
         self.assertEqual(mock_writer.write.call_count, len(self.video_frames))
 
-    @patch('cv2.imwrite')
-    @patch('mio.models.frames.VideoWriter.init_video')
+    @patch("cv2.imwrite")
+    @patch("mio.models.frames.VideoWriter.init_video")
     def test_invalid_frame_type_raises_exception(self, mock_init_video, mock_imwrite):
         # Test with an invalid type (e.g., integer)
         with self.assertRaises(ValueError):
@@ -61,5 +65,6 @@ class TestNamedFrame(unittest.TestCase):
         mock_imwrite.assert_not_called()
         mock_init_video.assert_not_called()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
