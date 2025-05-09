a257d60400c51db27ea58796c8a1404e85868da1
semwal <semwal@g.ucla.edu>
2025-05-09 16:46:53 -0700

    Changed class GSDevConfig to pass
    changed class GSDevConfig to pass only, bypassing the pixel depth value of 10

--- mio/devices/gs/config.py
+++ mio/devices/gs/config.py
@@ -1,11 +1,9 @@
-from typing import Annotated, Literal as L
+from typing import Annotated
 from annotated_types import Len
 from mio.models.stream import StreamDevConfig
 
 class GSDevConfig(StreamDevConfig):
-    preamble: Annotated[bytes, Len(min_length=12, max_length=12)]
-    """Example docstring"""
-    pix_depth: L[10] = 10
-
-    # processing header from buffers, see _parse_header
-    # header_len
+    # preamble: Annotated[bytes, Len(min_length=12, max_length=12)]
+    # """Example docstring"""
+    # pix_depth: L[10] = 10
+    pass
