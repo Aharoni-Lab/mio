87b94c629a4980e8122bc754620d7c0bc9ce525b
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-07-01 21:56:06 -0700

    extract fpga_recv buffer splitting logic to separate iterator function, untested.

--- mio/devices/mocks.py
+++ mio/devices/mocks.py
@@ -10,8 +10,14 @@ Not to be considered part of the public interface of mio <3
 # ruff: noqa: D102
 
 import os
+import sys
 from pathlib import Path
 from typing import Dict, Optional
+
+if sys.version_info < (3, 11):
+    from typing_extensions import Self
+else:
+    from typing import Self
 
 from mio.exceptions import EndOfRecordingException
 
@@ -32,7 +38,12 @@ class okDevMock:
     this mock is to be used within a separate process.
     """
 
-    def __init__(self, serial_id: str = ""):
+    def __init__(
+        self,
+        read_length: int,
+        serial_id: str = "",
+    ):
+        self.read_length = read_length
         self.serial_id = serial_id
         self.bit_file: Optional[Path] = None
 
@@ -60,7 +71,12 @@ class okDevMock:
         assert Path(bit_file).exists()
         self.bit_file = Path(bit_file)
 
-    def read_data(self, length: int, addr: int = 0xA0, blockSize: int = 16) -> bytearray:
+    def read_data(
+        self, length: int | None = None, addr: int = 0xA0, blockSize: int = 16
+    ) -> bytearray:
+        if length is None:
+            length = self.read_length
+
         if self._buffer_position >= len(self._buffer):
             # Error if called after we have returned the last data
             raise EndOfRecordingException("End of sample buffer")
@@ -72,3 +88,12 @@ class okDevMock:
 
     def set_wire(self, addr: int, val: int) -> None:
         self._wires[addr] = val
+
+    def __iter__(self) -> Self:
+        return self
+
+    def __next__(self) -> bytes:
+        try:
+            return self.read_data()
+        except EndOfRecordingException as e:
+            raise StopIteration() from e
