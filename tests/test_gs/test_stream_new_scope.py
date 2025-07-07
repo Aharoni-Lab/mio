51a884fe3556d9cabd5c6521980521bdd328db7e
semwal <semwal@g.ucla.edu>
2025-07-07 14:02:22 -0700

    Timeout for debug
    Previous timeout for debug set to 60 s

--- tests/test_gs/test_stream_new_scope.py
+++ tests/test_gs/test_stream_new_scope.py
@@ -1,30 +1,14 @@
-import re
-from pathlib import Path
 
-import multiprocessing
-import os
-import pytest
-import pandas as pd
-import sys
-import signal
-import time
-from contextlib import contextmanager
-
-from mio import BASE_DIR
-from mio.devices.gs import testing
 from mio.devices.gs.daq import GSStreamDaq
-from mio.devices.gs.config import GSDevConfig
 
-from mio.devices.gs.header import GSBufferHeaderFormat, GSBufferHeader
-import numpy as np
-from mio.utils import hash_video, hash_file
-from ..conftest import DATA_DIR, CONFIG_DIR
+from mio.utils import hash_file
+from ..conftest import DATA_DIR
 
 from mio.stream_daq import StreamDaq
 
 
-def test_binary_output(config, data, set_okdev_input, tmp_path):
-    daqConfig = GSStreamDaq.from_id(config)
+def test_binary_output(set_okdev_input, tmp_path):
+    daqConfig = GSStreamDaq.from_id("MSUS-test")
 
     data_file = DATA_DIR / "test_new_scope.bin"
     set_okdev_input(data_file)
