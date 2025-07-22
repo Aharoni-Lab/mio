3665fd0028e47388326bb96719916fd6166e57f8
t-sasatani <sasatani.dev@gmail.com>
2025-07-21 21:46:09 -0700

    assert process with empty method does nothing

--- tests/test_process/test_video.py
+++ tests/test_process/test_video.py
@@ -54,6 +54,15 @@ def test_noise_patch_processor_no_config(random_8bit_video_frame, tmp_path):
     with pytest.raises(ValueError):
         NoisePatchProcessor("denoise_example", denoise_config.noise_patch, tmp_path)
 
+def test_noise_patch_processor_no_methods(random_8bit_video_frame, tmp_path):
+    denoise_config = DenoiseConfig.from_id("denoise_example")
+    denoise_config.noise_patch.enable = True
+    denoise_config.noise_patch.method = []
+
+    processor = NoisePatchProcessor("denoise_example", denoise_config.noise_patch, tmp_path)
+    processed_frame = processor.process_frame(random_8bit_video_frame)
+    assert processed_frame is random_8bit_video_frame
+
 def test_freqency_mask_processor(video_frame, tmp_path):
     denoise_config = DenoiseConfig.from_id("denoise_example")
     denoise_config.frequency_masking.enable = True
