Close to getting tests and fixing size calculation bugs
1. config files are not set to the final (processed) version of the camera output
2. test files have trouble constructing a frame
3.

Results: Here are the changes I (and Jonny) made in MSUS Mio today

1. Changed original config files
    1. MSUS and MSUS-test
2. Changed npix calculation
    1. daq.py—> buffer_npix
3. Added config modification files to ensure all data is inputted correctly
    1. buffer_npix calles self.config.frame_width_input
4. Changed
5. Corrected errors
    1. This includes buffer2array not returning data
6. Added test_daq

Todo:
Here are the things that I am working on:
1. fix the test to calculate the final processing is working

mio/devices/gs/daq.py
@@ -21,11 +21,12 @@ def format_frame(frame_data: list[np.ndarray], config: GSDevConfig) -> np.ndarra
     Convert a list of 1D pixel arrays into a full frame, stripping the leading "training" pixels
     """
     pixels = np.concatenate(frame_data)  # concatenates to 1xn
-    frame = pixels.reshape((config.frame_height, config.frame_width_input))
+    frame = pixels.reshape((config.frame_height, config.frame_width))
     # strip training pixels
     frame = frame[:, 8:]
 
     return frame
+
 
 
 class GSStreamDaq(StreamDaq):
@@ -65,8 +66,23 @@ class GSStreamDaq(StreamDaq):
         self._buffered_writer: Optional[BufferedCSVWriter] = None
         self._header_plotter: Optional[StreamPlotter] = None
 
+    @property
+    def buffer_npix(self) -> list[int]:
+        """List of pixels per buffer for a frame includes unprocessed data"""
+        if self._buffer_npix is None:
+            total_pixels = self.config.frame_width_input * self.config.frame_height
+            buffer_npix = [self.config.max_pixels_per_buffer] * np.ceil(total_pixels / self.config.max_pixels_per_buffer)
+            remainder = total_pixels % self.config.max_pixels_per_buffer
+            if remainder != 0:
+                buffer_npix[-1] = remainder
+            self._buffer_npix = buffer_npix
+        return self._buffer_npix
+
     def _format_frame_inner(self, frame_data: list[np.ndarray]) -> np.ndarray:
         return format_frame(frame_data, self.config)
+
+
+
 
     def _handle_frame(
         self,

mio/devices/gs/header.py
@@ -38,7 +38,7 @@ def buffer_to_array(buffer: bytes) -> np.ndarray:
     padded = np.pad(stripped, ((0, 0), (6, 0)), mode="constant", constant_values=0)
     packed_16bit = np.packbits(padded, axis=1).view(np.uint16).byteswap()
 
-    return packed_16bit.flatten()
+    return packed_16bit.flatten() # original
 
 
 class GSBufferHeader(StreamBufferHeader):
@@ -66,13 +66,12 @@ class GSBufferHeader(StreamBufferHeader):
     ) -> tuple[Self, np.ndarray]:
         """Split buffer into a :class:`.GSBufferHeader` and a 1D, 16-bit pixel array."""
         header_start = len(config.preamble)
-        header_end =  header_start + ((header_fmt.header_length)*4) # = 12, also shows incorrect header values
-         # 44 ((384-32)/32)  = 11
+        header_end =  header_start + ((header_fmt.header_length)*4) # = 44 ((384-32)/32)  = 11
         header_array = np.frombuffer(buffer[header_start:header_end], dtype=np.uint32)
-        print(header_end, header_array)
+        # print(header_end, header_array)
         header = cls.from_format(header_array, header_fmt, construct=True)
         payload = buffer_to_array(buffer[header_end:])
-        print(len(header_array), len(buffer[header_end:]))
+        # print(len(header_array), len(buffer[header_end:]))
         # breakpoint()
         return header, payload
 