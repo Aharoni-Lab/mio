031a810b032e0c7f99f040cfb9cbc1be58a72d58
t-sasatani <sasatani.dev@gmail.com>
2025-10-02 11:34:50 +0900

    Purely test the skvideo.io itself

---
+++ tests/test_skvideo.py
@@ -0,0 +1,34 @@
+"""
+Test for skvideo.io.FFmpegWriter.
+"""
+
+from skvideo.io import FFmpegWriter
+import numpy as np
+import cv2
+
+def test_write_video(tmp_path):
+    """
+    Write a random grayscale video and check the number of frames.
+    Check with OpenCV that the video is written correctly.
+    """
+
+    width, height, num_frames, fps = 200, 200, 200, 20
+    output_dict = {
+        "-vcodec": "rawvideo",
+        "-f": "avi",
+        "-filter:v": "format=gray",
+        "-r": str(fps),
+    }
+
+    out = tmp_path / "test.avi"
+    writer = FFmpegWriter(str(out), outputdict=output_dict)
+    for _ in range(num_frames):
+        frame = np.random.randint(0, 256, (height, width), dtype=np.uint8)
+        writer.writeFrame(frame)
+    writer.close()
+
+    cap = cv2.VideoCapture(str(out))
+    avi_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.release()
+
+    assert avi_frames == num_frames
